8690a79927616bf3b684a2ebc0580ed51176f870
tuyaarak <kobrat-22@mail.ru>
2022-12-28 19:39:43 +0900

    Мой второй puch "тернарный оператор"

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,23 @@ public class Main {
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int max = a > b ? a : b; // тернарный оператор
+        String result = a == b ? "equal"  :
+                       a > b ? "more" : "Less";
+        System.out.println(result);
 
-
-        // комментировать Ctrl + Shift + /
-
-        /* if (a > b ) {
-            max = a;
-
+        if (a == b) {
+            result - "равно";
+        } else if (a > b) {
+            result = "больше";
         } else {
-            max = b;
-          }+/
+            result = "меньше";
+        }
+        // второй случай использования тернарного оператора - end
 
-       // тернарный оператор - start
-       // result = condition ? trueCase : elseCase;
+        // тернарный оператор - start
+                           if (10 > 9)           "выражение верно"      "выражение не верно"
+
+       //  String result = выражение сравнения ? сли верно выражение : если выражение не верно;
 
        // тернарный оператор - end
 
